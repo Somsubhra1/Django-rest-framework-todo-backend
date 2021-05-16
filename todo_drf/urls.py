19739a10c0af4a6c032b67d9d25801bcd9c5bcd0
Somsubhra Das <das.somsubhra1@gmail.com>
2021-05-16 13:06:02 +0530

    added home route

--- todo_drf/urls.py
+++ todo_drf/urls.py
@@ -17,8 +17,10 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
+from . import views
 
 urlpatterns = [
+    path("", views.home, name="home"),
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
 ]
