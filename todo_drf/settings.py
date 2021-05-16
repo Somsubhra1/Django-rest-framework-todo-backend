0d7fcb532fa55c666c307ca1d149b1b7010219b7
Somsubhra Das <das.somsubhra1@gmail.com>
2021-05-16 12:53:31 +0530

    added static files

--- todo_drf/settings.py
+++ todo_drf/settings.py
@@ -126,7 +126,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
